ef28c000f7d77e37c2893c14bd7932f90b75bc26
Ke-vin-S <kevinxsanjula@gmail.com>
2025-04-22 10:35:26 +0530

    .

--- logistics_core/settings.py
+++ logistics_core/settings.py
@@ -53,7 +53,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'logisticsCore.urls'
+ROOT_URLCONF = 'logistics_core.urls'
 
 TEMPLATES = [
     {
